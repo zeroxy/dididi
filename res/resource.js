c054d54a5513c4554a4ba32badec03b64e3f0c49
김광진 <kjink.kim@samsung.com>
2015-09-14 18:12:15 +0900

    hello world!!

---
+++ res/resource.js
@@ -0,0 +1,8 @@
+var grave       = "grave.png";
+var garlic         = "garlic.png";
+var shovel      = "shovel.png";
+var g_resources = [
+  grave,
+  garlic,
+  shovel
+];
---
+++ src/titleApp.js
@@ -0,0 +1,41 @@
+var TitleLayer = cc.Layer.extend({
+  helloLabel:null,
+  sprite:null,
+
+  init:function () {
+
+    this._super();
+
+    var size = cc.director.getWinSize();
+
+    cc.MenuItemFont.setFontSize(25);
+    cc.MenuItemFont.setFontName("Impact");
+    var closeItem = new cc.MenuItemFont.create(
+      "Game Start !",
+      function () {
+        var scene = new GameScene();
+        cc.director.runScene(scene);
+      },this);
+    closeItem.setAnchorPoint(0.5, 0.5);
+    closeItem.setPosition(size.width /2, size.height /2 -50);
+    
+    var menu = new cc.Menu(closeItem);
+    menu.setPosition(0, 0);
+    this.addChild(menu, 1);
+    
+
+    this.helloLabel = new cc.LabelTTF("Catch The FLYYY!", "Impact", 38);
+    this.helloLabel.setPosition(size.width / 2, size.height /2  + 40);
+    this.addChild(this.helloLabel, 5);
+
+  }
+});
+
+var TitleScene = cc.Scene.extend({
+  onEnter:function () {
+    this._super();
+    var layer = new TitleLayer();
+    this.addChild(layer);
+    layer.init();
+  }
+});
